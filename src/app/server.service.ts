3c96615d571613fd6e8d54fee176ba73f0dfa9dd
Robert Lowe <robertmlowe@rmlowe.com>
2019-04-30 13:02:51 +0800

    Sending Requests (Example: POST Request)

---
+++ src/app/server.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+
+@Injectable()
+export class ServerService {
+  constructor(private http: Http) {}
+  storeServers(servers: any[]) {
+    return this.http.post('https://udemy-ng-http-df9d8.firebaseio.com/data.json', servers);
+  }
+}
